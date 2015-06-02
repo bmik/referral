e1dc59b764325573264d36cb3d30c19f61a98f07
Bartlomiej Mike <Bartlomiej.Mike@ericpol.com>
2015-06-02 22:05:30 +0200

    Maven project structure

---
+++ Referral/src/main/java/edu/uek/referral/App.java
@@ -0,0 +1,13 @@
+package edu.uek.referral;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
