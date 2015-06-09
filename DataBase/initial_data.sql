12b8bbae74650674e906c009b69c60acc8a077ee
Bartlomiej Mike <Bartlomiej.Mike@ericpol.com>
2015-06-09 22:31:11 +0200

    a

---
+++ DataBase/initial_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO examination (id, code, name) VALUES (1, '100', 'Morfologia krwi');
+INSERT INTO examination (id, code, name) VALUES (2, '102', 'Posiew');
+INSERT INTO examination (id, code, name) VALUES (3, '110', 'Wymaz z ucha');
+INSERT INTO examination (id, code, name) VALUES (4, '200', 'Wymaz z gardła');
+INSERT INTO examination (id, code, name) VALUES (5, '280', 'Sód');
+INSERT INTO examination (id, code, name) VALUES (6, '282', 'Potas');
+INSERT INTO examination (id, code, name) VALUES (7, '301', 'Kreatynina');
+INSERT INTO examination (id, code, name) VALUES (8, '302', 'Glukoza');
